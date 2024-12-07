58ee701248b500495effd3be211ec549b4fcd69f
SebastianTorresG <123436366+SebastianTorresG@users.noreply.github.com>
2024-12-07 12:05:34 -0500

    arreglos menú Eestudiante

--- src/gui/MenuEstudiante.java
+++ src/gui/MenuEstudiante.java
@@ -3,6 +3,18 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SpringLayout;
+import javax.swing.JTextField;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class MenuEstudiante {
 
@@ -38,6 +50,26 @@ public class MenuEstudiante {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("Menú Estudiante");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JMenu mnNewMenu = new JMenu("Learning Paths Inscritos");
+		panel.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Learning Path");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenu mnNewMenu_1 = new JMenu("Todos los Learning Paths");
+		panel.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Learning Path");
+		mnNewMenu_1.add(mntmNewMenuItem_1);
 	}
 
 }
