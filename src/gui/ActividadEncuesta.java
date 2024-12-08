259a089237de7164e1f4a46afceb11224ace2f54
SebastianTorresG <123436366+SebastianTorresG@users.noreply.github.com>
2024-12-08 10:19:22 -0500

    ActividadEncuesta esqueleto

---
+++ src/gui/ActividadEncuesta.java
@@ -0,0 +1,77 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
+
+public class ActividadEncuesta extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ActividadEncuesta frame = new ActividadEncuesta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ActividadEncuesta() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Encuesta");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JButton btnNewButton = new JButton("Entregar");
+		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+		
+		JButton btnNewButton_1 = new JButton("Calificar Encuesta");
+		contentPane.add(btnNewButton_1, BorderLayout.EAST);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.WEST);
+		
+		JComboBox comboBox = new JComboBox();
+		panel.add(comboBox);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("Pregunta x");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(lblNewLabel_1, BorderLayout.NORTH);
+		
+		JTextArea textArea = new JTextArea();
+		panel_1.add(textArea, BorderLayout.CENTER);
+	}
+
+}
